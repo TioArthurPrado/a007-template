f09f9145d1505c2e83ee8020ec5c25aea47f18a0
Arthur Prado <arthur.matheus.prado@gmail.com>
2022-07-21 18:38:43 -0300

    TESTE

---
+++ script.js
@@ -0,0 +1,27 @@
+const nome = ("Arthur")
+let computadores = (2)
+let sãoPaulo = true   
+
+console.log(typeof nome)
+console.log(typeof computadores)
+console.log(typeof sãoPaulo)
+
+console.log("Olá, meu nome é", nome, "eu tenho", computadores, "computadores", "e eu moro em", sãoPaulo)
+
+let a 
+let b 
+let c
+
+a = 20
+b = 30
+c;
+
+a = 30 
+b = 20
+c;
+console.log(a,b,c)
+
+c=a
+a=b
+b=c
+console.log(a,b)
